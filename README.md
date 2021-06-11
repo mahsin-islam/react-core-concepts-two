fcf00f606dc37a1cceddaeec83f422e69de7c295
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2021-06-11 20:42:53 +0600

    core concepts

--- README.md
+++ README.md
@@ -1,71 +1,14 @@
-# Getting Started with Create React App
-
+# Learn Some React Core Concepts In this demo project
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
-# react-core-concepts-two
+1. First how to declare components at react and how is looks a component.
+2. Component is similar in look and feel but different in data.
+3. Here we declare and use components in 3 different way:
+    a. simple write a name of component
+    b. If there in collection of data just after create map and passing the value
+    c. If their in any object that pass the object and use as a props or read as props
+4. Learn about state how to change data or where data could be changed.
+5. learn about click handler or event handler (like onClick,onMouseEvent)
+6. How to load dynamic data by using useEffect().
--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const nayoks = ['Anwar', 'Jafor', 'Salman', 'Razzak'];
+  const products = [
+    { name: 'Photoshop', price: '$90.99' },
+    { name: 'Illustrator', price: '$60.99' },
+    { name: 'Pdf Reader', price: '$6.99' }
+  ];
+
+  const friends = [
+    // { name: 'Abul Kalam', age: '11', profile: 'https://i.ibb.co/hKXtvy6/960x0.jpg' },
+    { name: 'Abul Kalam', age: '11', profile: 'https://www.w3schools.com/howto/img_avatar.png' },
+    { name: 'Hasnat Kaiser', age: '12', profile: 'https://i.ibb.co/10SYccm/1552313010-354215-noticia-normal.jpg' },
+    { name: 'Raji', age: '13', profile: 'https://i.ibb.co/dcXZfXL/Kevin-de-Bruyne-AP-3-571-855.jpg' }
+  ];
+  const nayokNames = nayoks.map(nayok => nayok);
+  console.log(nayokNames);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am a React Person</p>
+        <Counter></Counter>
+        <Users></Users>
+        {/* {
+          friends.map(friend => <Friends friend={friend}></Friends>)
+        } */}
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ul>
+        {
+          products.map(product => <Product product={product}></Product>)
+        }
+        {/* <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product> */}
+        {/* <Person name={nayoks[0]} naiyka="Mousumi"></Person>
+        <Person name="Jasim" naiyka="Shabana"></Person>
+        <Person name="Bappa Raj" naiyka="Cheka"></Person> */}
+        <Person name="Munna" job="football"></Person>
+        <Person name="Masum" job="dorshok"></Person>
       </header>
     </div>
   );
 }
 
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[])
+  return(
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.phone}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+function Counter(){
+  const [count, setCount] = useState(10);
+  const handleIncrease = () => setCount(count + 1);
+  // const handleIncrease =() => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // };
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onMouseMove={() => setCount(count -1)}>Decrease</button>
+      {/* <button onClick={handleIncrease}>Increased</button> */}
+      <button onClick={() => setCount(count + 1)}>Increased</button>
+    </div>
+  )
+}
+function Friends(props) {
+  const { name, age, profile } = props.friend;
+  return (
+    <div className="card">
+      <img src={profile} alt="Avatar" ></img>
+      {/* <img src="img_avatar.png" alt="Avatar" style="width:100%" /> */}
+      <div className="container">
+        <h4><b>{name}</b></h4>
+        <p>Age: {age}</p>
+      </div>
+    </div>
+  )
+}
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  const { name, price } = props.product;
+  console.log(name, price);
+  return (
+    <div style={productStyle}>
+      <h4>{name}</h4>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+// function Person(props) {
+//   const personStyle = {
+//     border:'2px solid red',
+//     margin:'10px'
+//   }
+//   console.log(props);
+//   return <div style={personStyle}>
+//   <h1>Nayok: {props.name}</h1>
+//   <h3>Hero of {props.naiyka}</h3>
+//   </div> 
+// }
+
+function Person(props) {
+  return (
+    <div style={{ border: '2px solid goldenrod', margin: '10px', width: '400px' }}>
+      <h3>My name: {props.name} </h3>
+      <p>My Profession: {props.job}</p>
+    </div>
+  )
+}
 export default App;
